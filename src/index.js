fd7608cf03a254563e2f35268bf6cc16eda191cb
Gabriel <knuckles4x@gmail.com>
2020-09-11 00:02:17 -0300

    create new url logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const path = require("path");
+const yup = require("yup");
+const { nanoid } = require("nanoid");
 
 const app = express();
 
@@ -17,8 +19,38 @@ app.get("/:id", (req, res) => {
   // TODO: redirect to url
 });
 
-app.post("/url", (req, res) => {
-  // TODO: create url
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[a-z0-9_\-]/i),
+  url: yup.string().trim().url().required(),
+});
+
+app.post("/url", async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({ slug, url });
+    if (!slug) {
+      slug = nanoid(5);
+    }
+    slug = slug.toLowerCase();
+    res.json({ slug, url });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "?XD" : error.stack,
+  });
 });
 
 app.get("/url/:id", (req, res) => {
