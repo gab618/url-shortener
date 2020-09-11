26c96892b4cb5652a8eb0fa632058d6611d31d78
Gabriel <knuckles4x@gmail.com>
2020-09-10 23:43:02 -0300

    static html page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const path = require("path");
 
 const app = express();
 
@@ -10,8 +11,18 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/:id", (req, res) => {
+  // TODO: redirect to url
+});
+
+app.post("/url", (req, res) => {
+  // TODO: create url
+});
+
+app.get("/url/:id", (req, res) => {
+  // TODO: get a shor url by id
 });
 
 const port = process.env.PORT || 3333;
